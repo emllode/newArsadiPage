6674982a42c07df0a738e684e5248f40e4efe80b
Christopher Bergman <cb.christopherbergman@gmail.com>
2021-02-03 21:07:29 +0100

    fixed a bunch of stuff.

--- app/js/script.js
+++ app/js/script.js
@@ -1,7 +1,44 @@
-$( '.js-input' ).keyup(function() {
-    if( $(this).val() ) {
-       $(this).addClass('not-empty');
-    } else {
-       $(this).removeClass('not-empty');
-    }
-  }); 
+jQuery(document).ready(function($) {
+  // Scroll to the desired section on click
+  // Make sure to add the `data-scroll` attribute to your `<a>` tag.
+  // Example: 
+  // `<a data-scroll href="#my-section">My Section</a>` will scroll to an element with the id of 'my-section'.
+  function scrollToSection(event) {
+    event.preventDefault();
+    var $section = $($(this).attr('href')); 
+    $('html, body').animate({
+      scrollTop: $section.offset().top
+    }, 700);
+  }
+  $('[data-scroll]').on('click', scrollToSection);
+}(jQuery));
+
+
+
+
+
+$(document).ready(function(){
+    $("h3").delay("1000").fadeIn();
+    // hide #back-top first
+    $("#back-top").hide();
+    // fade in #back-top
+    $(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 400) {
+                $('#back-top').fadeIn();
+            } else {
+                $('#back-top').fadeOut();
+            }
+        });
+
+        // scroll body to 0px on click
+        $('a#back-top').click(function () {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+    });
+
+
+});
