68a606f2b5323346bc34a655d3d56a804e526234
Christopher Bergman <cb.christopherbergman@gmail.com>
2021-02-26 17:37:34 +0100

    fixed changes that was asked.

--- app/js/script.js
+++ app/js/script.js
@@ -72,3 +72,18 @@ $(document).ready(function () {
 		});
 	});
 });
+
+window.onscroll = function () {
+	myFunction();
+};
+
+var navbar = document.querySelector('.navbarMenu');
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+	if (window.pageYOffset >= sticky) {
+		navbar.classList.add('sticky');
+	} else {
+		navbar.classList.remove('sticky');
+	}
+}
